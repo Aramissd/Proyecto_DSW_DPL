4f54eeaf669fb8ef238ba71919ec2a6cbab3c930
Aramis Sarmiento Díaz <aramiissd@gmx.es>
2023-01-31 16:25:34 +0000

    d

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php include "/parts/header_index.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" http-equiv="refresh">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#">Aramis Blog</a>
