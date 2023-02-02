0f105bb2ce0c3a6a9db761aceb86c33db07de11d
Aramis Sarmiento Díaz <aramiissd@gmx.es>
2023-02-02 01:30:08 +0000

    xc

--- parts/header_login.php
+++ parts/header_login.php
@@ -2,12 +2,10 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Aramis Blog</title>
-	
+    <title>Aramis Blog</title>	
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" http-equiv="refresh"> 
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"> 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" href="../../public/build/css/custom.css">
